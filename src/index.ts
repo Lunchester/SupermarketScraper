9388f42483de5d6ea6951d5c1d35f83dc34f4300
Tobin Stultiens <tobin.stultiens@gmail.com>
2019-10-25 16:02:33 +0200

    changed request to request-promise, Acess is still getting denied

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-const request = require("request");
+const rp = require('request-promise');
 const cheerio = require("cheerio");
 
-const plussearch = "https://www.plus.nl/zoekresultaten?SearchTerm=Boterham";
+const plussearch = "https://www.plus.nl/zoekresultaten?SearchTerm=";
 
-function getFull(callback: any) {
-    request.get(plussearch, function (
-        error: any,
-        response: any,
-        data: any
-    ) {
-        const $ = cheerio.load(data);
-        callback(error, {
-            story: $("")
-                .text()
-                .trim()
-        });
-        console.log($.html());
+function getFull(search: string) {
+    var searchres = plussearch + search;
+    rp({uri :searchres, headers:{'User-Agent': 'Mozilla/5.0'}})
+    .then(function (htmlString: any) {
+        // Process html...
+        console.log(htmlString);
+    })
+    .catch(function (err: any) {
+        // Crawling failed...
+        console.log(err);
     });
-}
+};
+
+getFull("Boterham");
