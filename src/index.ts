ca921497202afa063abf0b8382477c62b0e86d44
Tobin Stultiens <tobin.stultiens@gmail.com>
2019-11-07 14:28:40 +0100

    You can retrieve the objects now

--- src/index.ts
+++ src/index.ts
@@ -7,45 +7,40 @@ import { IProductModel } from "./models/product-model";
 
 let productList: IProductModel[] = []
 
-function RetrieveSearchInformation(SearchTerm: String): IProductModel[] {
-    puppeteer
-        .launch()
-        .then(function (browser: { newPage: () => void; }) {
-            return browser.newPage();
-        })
-        .then(function (page: { goto: (arg0: string) => { then: (arg0: () => any) => void; }; content: () => void; }) {
-            return page.goto(url + SearchTerm).then(function () {
-                return page.content();
-            });
-        })
-        .then(function (html: any) {
-            var descriptions: string[] = [];
-            var images: string[] = [];
-            $('a.product-tile > div.product-tile__info > p.product-tile__description', html).each(function (i: number, elem: string) {
-                //Displays the product info
-                descriptions[i] = $(elem).text();
-            });
-            $('div.prod-tile', html).each(function (i: number, elem: string) {
-                //Displays the product data
-                images[i] = imageUrlFormatBegin + $(elem).attr('data-id') + imageUrlFormatEnd;
-            });
-            for (let index = 0; index < descriptions.length; index++) {
-                //console.log(descriptions[index])
-                //console.log(images[index])
-                let product: IProductModel = {
-                    description: descriptions[index],
-                    imgLink: images[index]
-                }
-                productList[index] = product;
-                console.log(productList);
-            }
-            return productList;
-        })
-        .catch(function (err: any) {
-            console.log(err);
-            return null;
-        });
+async function RetrieveSearchInformation(SearchTerm: String): Promise<IProductModel[]> {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url + SearchTerm);
+    await page.content();
+    const bodyHandle = await page.$('body');
+    const html = await page.evaluate((body: { innerHTML: any; }) => body.innerHTML, bodyHandle);
+    await ReadData(html);
     return productList;
 }
+
+function ReadData(html: any) {
+    var descriptions: string[] = [];
+    var images: string[] = [];
+    $('a.product-tile > div.product-tile__info > p.product-tile__description', html).each(function (i: number, elem: string) {
+        //Displays the product info
+        descriptions[i] = $(elem).text();
+    });
+    $('div.prod-tile', html).each(function (i: number, elem: string) {
+        //Displays the product data
+        images[i] = imageUrlFormatBegin + $(elem).attr('data-id') + imageUrlFormatEnd;
+    });
+    for (let index = 0; index < descriptions.length; index++) {
+        //console.log(descriptions[index])
+        //console.log(images[index])
+        let product: IProductModel = {
+            description: descriptions[index],
+            imgLink: images[index]
+        }
+        productList[index] = product;
+    }
+    return productList;
+};
 let result = RetrieveSearchInformation("brood");
-console.log(result);
+result.then(function (result) {
+    console.log(result);
+})
