af4022810f5b7a88c98dcec5041c39831904b449
Tobin Stultiens <tobin.stultiens@gmail.com>
2019-11-07 14:12:27 +0100

    created a working version for storing the information. The only issue is that it's not waiting for it to completer

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const imageUrlFormatEnd = '.png';
 const $ = require('cheerio');
 import { IProductModel } from "./models/product-model";
 
-var productList: IProductModel[] = [];
+let productList: IProductModel[] = []
 
 function RetrieveSearchInformation(SearchTerm: String): IProductModel[] {
     puppeteer
@@ -19,21 +19,33 @@ function RetrieveSearchInformation(SearchTerm: String): IProductModel[] {
             });
         })
         .then(function (html: any) {
-            $('a.product-tile > div.product-tile__info > p.product-tile__description', html).each(function (this: any) {
+            var descriptions: string[] = [];
+            var images: string[] = [];
+            $('a.product-tile > div.product-tile__info > p.product-tile__description', html).each(function (i: number, elem: string) {
                 //Displays the product info
-                // = $(this).text();
-                console.log($(this).text());
+                descriptions[i] = $(elem).text();
             });
-            $('div.prod-tile', html).each(function (this: any) {
-                //Displays the product info
-                console.log(imageUrlFormatBegin + $(this).attr('data-id') + imageUrlFormatEnd);
+            $('div.prod-tile', html).each(function (i: number, elem: string) {
+                //Displays the product data
+                images[i] = imageUrlFormatBegin + $(elem).attr('data-id') + imageUrlFormatEnd;
             });
+            for (let index = 0; index < descriptions.length; index++) {
+                //console.log(descriptions[index])
+                //console.log(images[index])
+                let product: IProductModel = {
+                    description: descriptions[index],
+                    imgLink: images[index]
+                }
+                productList[index] = product;
+                console.log(productList);
+            }
             return productList;
         })
         .catch(function (err: any) {
-            //handle error
+            console.log(err);
             return null;
         });
     return productList;
 }
-RetrieveSearchInformation("brood");
+let result = RetrieveSearchInformation("brood");
+console.log(result);
